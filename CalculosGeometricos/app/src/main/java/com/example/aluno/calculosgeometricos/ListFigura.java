d0cd815d4a6907953d733729d25603826df64ffe
willianIF <30538522+willianIF@users.noreply.github.com>
2018-11-29 00:05:07 -0200

    Rodando teste
    asdasd

---
+++ CalculosGeometricos/app/src/main/java/com/example/aluno/calculosgeometricos/ListFigura.java
@@ -0,0 +1,50 @@
+package com.example.aluno.calculosgeometricos;
+
+import android.app.Activity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ListFigura extends Activity {
+
+    public static final String EXTRA_NUMEROCURSO = "numerofigura";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_figura);
+
+        int numeroFigura = (Integer) getIntent().getExtras().get(EXTRA_NUMEROCURSO);
+
+        try{
+            SQLiteOpenHelper cursosDatabaseHelper = new CalculoDataBaseHelper(this);
+            SQLiteDatabase db = cursosDatabaseHelper.getReadableDatabase();
+
+            Cursor cursor = db.query("figuras",
+                    new String[] {"nome", "tipo"},
+                    "_id = ?",
+                    new String[] {Integer.toString(numeroFigura)},
+                    null, null, null);
+
+            if (cursor.moveToFirst()){
+                String valorNome = cursor.getString(0);
+                String valorTipo = cursor.getString(1);
+
+                TextView nome = (TextView) findViewById(R.id.nome);
+                nome.setText(valorNome);
+
+                TextView tipo = (TextView) findViewById(R.id.tipo);
+                nome.setText(valorTipo);
+            }
+            cursor.close();
+            db.close();
+        }catch (SQLiteException e) {
+            Toast toast = Toast.makeText(this, "DB indispoível", Toast.LENGTH_LONG);
+            toast.show();
+        }
+    }
+}
--- CalculosGeometricos/app/src/main/java/com/example/aluno/calculosgeometricos/ListFiguras.java
+++ CalculosGeometricos/app/src/main/java/com/example/aluno/calculosgeometricos/ListFiguras.java
@@ -1,12 +1,59 @@
 package com.example.aluno.calculosgeometricos;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
-public class ListFiguras extends AppCompatActivity {
+public class ListFiguras extends ListActivity {
+    private SQLiteDatabase db;
+    private Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ListView listFiguras = getListView();
+
+        try{
+            SQLiteOpenHelper figurasDatabaseHelper = new CalculoDataBaseHelper(this);
+            db = figurasDatabaseHelper.getReadableDatabase();
+
+            cursor = db.query("figuras",
+                    new String[]{"_id", "nome"},
+                    null, null, null, null,null);
+
+            CursorAdapter listAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1,
+                    cursor,
+                    new String[]{"nome"},
+                    new int[]{android.R.id.text1},
+                    0);
+
+            listFiguras.setAdapter(listAdapter);
+        }catch (SQLiteException e) {
+            Toast toast = Toast.makeText(this, "DB indisponível", Toast.LENGTH_LONG);
+            toast.show();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        cursor.close();
+        db.close();
+    }
+
+    @Override
+    public void onListItemClick(ListView listView, View itemView, int position, long id) {
+        Intent i = new Intent(this, ListFigura.class);
+        i.putExtra(ListFigura.EXTRA_NUMEROCURSO, (int) id);
+        startActivity(i);
     }
 }
